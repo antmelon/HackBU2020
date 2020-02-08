230332ce2e56cf5e10c0b27699faa7fc166f8a19
Anthony Longo <ferrari13@me.com>
2020-02-08 17:11:09 -0500

    created made okta login

--- app.py
+++ app.py
@@ -1,5 +1,52 @@
-from flask import Flask
+#Client ID 0oa24xlahiufsSAsx4x6
+#Client Secret NQu4W7_u6RHTFhRR6ggKkgTxMpZn07j2yCdsNWbo
+#Login token 00idk2KSs3hVXhksW2OBwKsaOVzJH6qVUF5OVPp8SL
+
+from flask import Flask, render_template, g, redirect, url_for
+from flask_oidc import OpenIDConnect
+from okta import UsersClient
+
 app = Flask(__name__)
+app.config["OIDC_CLIENT_SECRETS"] = "client_secrets.json"
+app.config["OIDC_COOKIE_SECURE"] = False
+app.config["OIDC_CALLBACK_ROUTE"] = "/oidc/callback"
+app.config["OIDC_SCOPES"] = ["openid", "email", "profile"]
+app.config["SECRET_KEY"] = "{{ LONG_RANDOM_STRING }}"
+app.config["OIDC_ID_TOKEN_COOKIE_NAME"] = "oidc_token"
+oidc = OpenIDConnect(app)
+okta_client = UsersClient("https://dev-270581.okta.com", "00idk2KSs3hVXhksW2OBwKsaOVzJH6qVUF5OVPp8SL")
+
+
+@app.before_request
+def before_request():
+    if oidc.user_loggedin:
+        g.user = okta_client.get_user(oidc.user_getfield("sub"))
+    else:
+        g.user = None
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/dashboard")
+@oidc.require_login
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/login")
+@oidc.require_login
+def login():
+    return redirect(url_for(".dashboard"))
+
+
+@app.route("/logout")
+def logout():
+    oidc.logout()
+    return redirect(url_for(".index"))
+
 
 @app.route("/textbooks")
 def getTextbooks():
