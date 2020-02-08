398fb599a4bd4c72361f94a57c3e6f5557289d81
Tyler Katz <tkatz1@binghamton.edu>
2020-02-08 17:45:34 -0500

    added forms

---
+++ forms.py
@@ -0,0 +1,16 @@
+from flask-wtf import Flask
+from wtfforms import StringField, PasswordField, SubmitField, BooleanField
+from wtfforms import DataRequired, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = submit('Sign Up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = submit('Login')
